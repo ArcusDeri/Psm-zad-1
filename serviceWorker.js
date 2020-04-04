4069a24112ffa652435e9051f08b016f0160eccc
Michal Wojtowicz <wojtowiczmichal97@gmail.com>
2020-04-04 11:37:19 +0200

    Added serviceWorker

---
+++ serviceWorker.js
@@ -0,0 +1,24 @@
+const staticBicycle = "bicycle-site-v1"
+const assets = [
+  "/index.html",
+  "/img/bike192.png",
+  "/img/bike256.png",
+  "/img/bike512.png",
+  "/img/bike144.png",
+]
+
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(staticBicycle).then(cache => {
+      cache.addAll(assets)
+    })
+  )
+})
+
+self.addEventListener("fetch", fetchEvent => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then(res => {
+      return res || fetch(fetchEvent.request)
+    })
+  )
+})
