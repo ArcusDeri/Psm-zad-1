4477e515ac86918b7265e344aa28f925f7a44792
Adam Zborowski <zborowskiadm@gmail.com>
2020-04-04 20:11:37 +0200

    resolve icon issue

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,11 +1,11 @@
 const staticBicycle = "bicycle-site-v1"
 const assets = [
-  "/index.html",
-  "/app.js",
-  "/img/bike192.png",
-  "/img/bike256.png",
-  "/img/bike512.png",
-  "/img/bike144.png",
+  "index.html",
+  "app.js",
+  "img/bike192.png",
+  "img/bike256.png",
+  "img/bike512.png",
+  "img/bike144.png",
 ]
 
 self.addEventListener("install", installEvent => {
